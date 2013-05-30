86ef7228205cae5a5c1486c9fd469b2ebd21f1fc
Daniel Marohn <daniel.marohn@gmail.com>
2013-05-30 12:35:46 +0200

    refactoring

--- sayNumber/say.py
+++ sayNumber/say.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import argparse
 import random
 from german import say, sayByExp
@@ -30,8 +29,15 @@ say.py --random --latinOnly --numeric 123
 217 vigintillionen 764 novendezilliarden 231 novendezillionen 953 oktodezilliarden 87 oktodezillionen ..."""
 
 
+class ArgFormatter(argparse.ArgumentDefaultsHelpFormatter):
+
+    def _format_usage(self, usage, actions, groups, prefix):
+        ret = super(argparse.ArgumentDefaultsHelpFormatter, self)._format_usage(usage, actions, groups, prefix)
+        return ret.replace("[number]", "number")
+
+
 def main(args):
-    number = args.number[0]
+    number = args.number
     if args.zeros:
         # Do not say given number, but the number with that many zeros.
         zeros, zerosLeft = divmod(number, 3)
@@ -39,10 +45,10 @@ def main(args):
         ret = ""
         plural = False
         if zerosLeft:
-            ret = "1%s " % "".join(["0" for _ in range(int(zerosLeft))])
+            ret = ("10" if zerosLeft == 1 else "100") + " "
             plural = True
         ret += sayByExp(zeros, plural)
-        numeric = "1" + "0" * number
+        numeric = "1" + "0" * number if args.numeric else None
     elif args.random:
         # Do not say given number, but a random number with that many digits.
         digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -60,25 +66,31 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Write german names of (very) big numbers, using long ladder system.',
-                                     formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     epilog=EXAMPLES)
-    parser.add_argument('--byLine', '-b', dest='byLine', action='store_true',
+                                     formatter_class=ArgFormatter,
+                                     epilog="Use -e to see examples.")
+    parser.add_argument('-e', '--example', dest='example', action='store_true',
+                        help='show examples and exit')
+    parser.add_argument('-b', '--byLine', dest='byLine', action='store_true',
                         help='Write components line by line.')
-    parser.add_argument('--latinOnly', '-l', dest='latinOnly', action='store_true',
+    parser.add_argument('-l', '--latinOnly', dest='latinOnly', action='store_true',
                         help='Say "123 millionen" instead of "einhundertdreiundzwanzigmillionen"')
-    parser.add_argument('--zeros', '-z', dest='zeros', action='store_true',
+    parser.add_argument('-z', '--zeros', dest='zeros', action='store_true',
                         help='Do not say given number, but the number with that many zeros.')
-    parser.add_argument('--random', '-r', dest='random', action='store_true',
+    parser.add_argument('-r', '--random', dest='random', action='store_true',
                         help='Do not say given number, but a random number with that many digits.')
-    parser.add_argument('--numeric', '-n', dest='numeric', action='store_true',
+    parser.add_argument('-n', '--numeric', dest='numeric', action='store_true',
                         help="Say the number also in numeric form.")
-    parser.add_argument('number', nargs=1, type=int, help='The number to say.')
+    parser.add_argument('number', nargs='?', type=int, default=-1, help='The number to say.')
 
     args = parser.parse_args()
+
     try:
+        if args.example:
+            parser.exit(message=EXAMPLES)
+        if args.number < 0:
+            raise ValueError("Missing argument number.")
         if args.zeros and args.random:
             raise ValueError("Options random and zeros cannot be combined.")
-
         main(args)
     except Exception as ex:
-        print ex.message
+        parser.error(ex.message)
